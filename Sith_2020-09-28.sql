16c15a2a6cd40846d151742d7e960a2aa0a4f49d
Tom Hinsley <tomhinsley@me.com>
2020-09-28 13:07:54 +0100

    added Sith SQL database

---
+++ Sith_2020-09-28.sql
@@ -0,0 +1,57 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.31)
+# Database: Sith
+# Generation Time: 2020-09-28 12:05:58 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table Sith
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Sith`;
+
+CREATE TABLE `Sith` (
+  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) DEFAULT NULL,
+  `homeworld` varchar(20) DEFAULT NULL,
+  `height` smallint(3) DEFAULT NULL,
+  `birthyear` tinyint(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `Sith` WRITE;
+/*!40000 ALTER TABLE `Sith` DISABLE KEYS */;
+
+INSERT INTO `Sith` (`id`, `name`, `homeworld`, `height`, `birthyear`)
+VALUES
+	(1,'Darth Vader','Tatooine',203,41),
+	(2,'Darth Maul','Dathomir',175,54),
+	(3,'Darth Sideous','Naboo',173,84),
+	(4,'Darth Tyranus','Serenno',193,102);
+
+/*!40000 ALTER TABLE `Sith` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
