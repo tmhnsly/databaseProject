ad8a52892b9846aea2185cf795bde28e258932d8
Tom Hinsley <tomhinsley@me.com>
2020-09-28 15:59:07 +0100

    added "top trumps card" structure to HTML and CSS

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
 
-    <title>Sithipedia</title>
+    <title>Sith-ipedia</title>
     <link href="normalize.css" type="text/css" rel="stylesheet">
     <link href="sithipedia.css" type="text/css" rel="stylesheet">
 </head>
@@ -13,6 +13,39 @@
     <header>
         <h1>Sith-ipedia</h1>
     </header>
+<main>
+<div class="characterDeck">
+    <div class="characterCard">
+        <h2 class="characterName">Name: </h2>
+        <p class="characterInfo">Homeworld: </p>
+        <p class="characterInfo">Height: </p>
+        <p class="characterInfo">Birth Year: </p>
+        <img src="">
+    </div>
+    <div class="characterCard">
+        <h2 class="characterName">Name: </h2>
+        <p class="characterInfo">Homeworld: </p>
+        <p class="characterInfo">Height: </p>
+        <p class="characterInfo">Birth Year: </p>
+        <img src="">
+    </div>
+    <div class="characterCard">
+        <h2 class="characterName">Name: </h2>
+        <p class="characterInfo">Homeworld: </p>
+        <p class="characterInfo">Height: </p>
+        <p class="characterInfo">Birth Year: </p>
+        <img src="">
+    </div>
+    <div class="characterCard">
+        <h2 class="characterName">Name: </h2>
+        <p class="characterInfo">Homeworld: </p>
+        <p class="characterInfo">Height: </p>
+        <p class="characterInfo">Birth Year: </p>
+        <img src="">
+    </div>
+</div>
+
+</main>
 </body>
 </html>
 
