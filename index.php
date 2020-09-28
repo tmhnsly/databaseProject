9e4706a45eed9db67b27dabbe65d1feede039f73
Tom Hinsley <tomhinsley@me.com>
2020-09-28 14:59:28 +0100

    added normlise and sithepedia CSS files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sithipedia</title>
+    <link href="normalize.css" type="text/css" rel="stylesheet">
+    <link href="sithipedia.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1>Sith-ipedia</h1>
+    </header>
+</body>
+</html>
+
